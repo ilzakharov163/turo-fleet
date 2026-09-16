4183fb9976193f49b9982f9cee8498880fc53358
ilzakharov163 <ilzakharov163@gmail.com>
2026-09-15 19:47:07 -0700

    Update layout.tsx

--- turo-fleet-archive/src/app/(app)/layout.tsx
+++ turo-fleet-archive/src/app/(app)/layout.tsx
@@ -12,9 +12,9 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     <ThemeProvider>
       <div className="flex min-h-screen" style={{ background: 'var(--background)' }}>
         <Sidebar />
-        <div className="flex-1 flex flex-col ml-[72px]">
+        <div className="flex-1 flex flex-col sm:ml-[72px]">
           <TopBar pathname={pathname} />
-          <main className="flex-1 p-6 overflow-auto">{children}</main>
+          <main className="flex-1 p-4 sm:p-6 pb-24 sm:pb-6 overflow-auto">{children}</main>
         </div>
       </div>
     </ThemeProvider>
